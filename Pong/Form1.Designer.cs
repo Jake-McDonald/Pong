4f4577534ec886411a94b87e08a769e6a3f56a0a
Jake McDonald <jacrilmcd@gmail.com>
2018-11-23 23:50:22 -0400

    Switched to using drawing instead of moving pictureBox

--- Pong/Form1.Designer.cs
+++ Pong/Form1.Designer.cs
@@ -29,27 +29,13 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 202);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(36, 32);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 30;
+            this.timer1.Interval = 16;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
@@ -57,19 +43,15 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(552, 494);
-            this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
     }
 }
--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,50 +21,87 @@ namespace Pong
         public Form1()
         {
             InitializeComponent();
+            this.SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, false);
+            this.SetStyle(ControlStyles.Opaque, false);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (ball.currentLocation.X > this.Width -40)
             {
-                Point offset = ball.bounce();
-                xVelocity = xVelocity * -1;
-                ball.MoveBall(4 * xVelocity + offset.X, 4 * yVelocity + offset.Y);
+                if (xVelocity > 0)
+                {
+                    Point offset = ball.bounce();
+                    xVelocity = xVelocity * -1;
+                    ball.MoveBall(4 * xVelocity + offset.X, 4 * yVelocity + offset.Y);
 
-                //ball.currentLocation = new Point(this.Width -200, this.Height / 2);
-                Debug.WriteLine("Changed velocity - Left");
+                    //ball.currentLocation = new Point(this.Width -200, this.Height / 2);
+                    Debug.WriteLine("Changed velocity - Left");
+                }
             }
             else if (ball.currentLocation.X < 0)
             {
-                Point offset = ball.bounce();
-                xVelocity = xVelocity * -1;
-                ball.MoveBall(4 * xVelocity + offset.X, 4 * yVelocity + offset.Y );
-                
-                //ball.currentLocation = new Point(10, this.Height / 2);
-                Debug.WriteLine("Changed velocity - Right");
+                if (xVelocity < 0)
+                {
+                    Point offset = ball.bounce();
+                    xVelocity = xVelocity * -1;
+                    ball.MoveBall(4 * xVelocity + offset.X, 4 * yVelocity + offset.Y);
+
+                    //ball.currentLocation = new Point(10, this.Height / 2);
+                    Debug.WriteLine("Changed velocity - Right");
+                }
             }
             else if(ball.currentLocation.Y > this.Height -80)
             {
-                yVelocity = yVelocity * -1;
+                if (yVelocity > 0)
+                {
+                    Point offset = ball.bounce();
+                    yVelocity = yVelocity * -1;
+                    ball.MoveBall(4 * xVelocity + offset.X, 4 * yVelocity + offset.Y);
+                }
 
             }
             else if(ball.currentLocation.Y < -20)
             {
-                yVelocity = yVelocity * -1;
+                if (yVelocity < 0)
+                {
+                    Point offset = ball.bounce();
+                    yVelocity = yVelocity * -1;
+                    ball.MoveBall(4 * xVelocity + offset.X, 4 * yVelocity + offset.Y);
+                }
             }
-            ball.MoveBall(4 * xVelocity, 2  * yVelocity);
-            pictureBox1.Location = ball.currentLocation;
-            Debug.WriteLine(pictureBox1.Location);
+            ball.MoveBall(2 * xVelocity, 1  * yVelocity);
+            //pictureBox1.Location = ball.currentLocation;
             Debug.WriteLine(ball.currentLocation);
 
-            Refresh();
-
+            this.Refresh();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             ball = new Ball(this.Width /2, this.Height / 2);
-            pictureBox1.Location = ball.currentLocation;
+            //pictureBox1.Location = ball.currentLocation;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.FillEllipse(Brushes.Red, new Rectangle(ball.currentLocation.X, ball.currentLocation.Y, 32, 32));
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+           
         }
     }
 
